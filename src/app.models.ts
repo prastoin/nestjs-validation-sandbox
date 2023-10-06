397322110e8cb2ce6bcdd525352f5b070461f044
prastoin <tkesray@gmail.com>
2023-10-06 11:09:58 +0200

    feat: setup swagger and apiProperty

--- src/app.models.ts
+++ src/app.models.ts
@@ -1,10 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsDefined, IsEnum, IsOptional } from "class-validator"
 
 export class PingRequestBody {
+    @ApiProperty({
+        enum: ["FOO", "FOU"]
+    })
     @IsDefined()
     @IsEnum({ "FOO": "FOO", "FOU": "FOU" })
     foo: ("FOO" | "FOU")
 
+    @ApiProperty({
+        enum: ["BAR", "BARRE"],
+        required: false,
+    })
     @IsOptional()
     @IsEnum({ "BAR": "BAR", "BARRE": "BARRE" })
     bar?: ("BAR" | "BARRE")
