817600feaf62f36210fa2e7c9b9d5d94a7eee4bf
prastoin <tkesray@gmail.com>
2023-10-10 19:51:22 +0200

    refactor: model query

--- src/app.models.ts
+++ src/app.models.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger"
-import { IsDefined, IsEnum, IsOptional } from "class-validator"
+import { Transform } from "class-transformer"
+import { IsArray, IsDefined, IsEnum, IsOptional } from "class-validator"
+import { fromArrayToRecord } from "./utils"
 
 export class PingRequestBody {
     @ApiProperty({
@@ -16,4 +18,49 @@ export class PingRequestBody {
     @IsOptional()
     @IsEnum({ "BAR": "BAR", "BARRE": "BARRE" })
     bar?: ("BAR" | "BARRE")
+}
+
+
+export const isoCode = ["JO", "KZ", "ZM", "ZW"] as const
+export type IsoCodeLiteral = typeof isoCode[number]
+const isCodeRecord = fromArrayToRecord([...isoCode])
+export class GetCountryCodeQuery {
+    @ApiProperty({
+        enum: isCodeRecord
+    })
+    @IsDefined()
+    @IsEnum(isCodeRecord)
+    code: IsoCodeLiteral
+}
+
+const countryList = ["EUR", "ASIA", "AMER"] as const;
+type CountryLiteral = typeof countryList[number];
+const countryRecord = fromArrayToRecord([...countryList])
+export class GetCountryRegionParam {
+    @ApiProperty({
+        enum: countryRecord
+    })
+    @IsEnum(countryRecord)
+    region: CountryLiteral
+}
+
+const languagesList = ["FRA", "ENG", "ITA"] as const
+type LanguageLiteral = typeof languagesList[number]
+const languagesRecord = fromArrayToRecord([...languagesList])
+export class GetLanguagesQuery {
+    @ApiProperty({
+        enum: languagesRecord,
+        type: [languagesRecord]
+    })
+    @Transform(({ value }) => {
+        console.log({ value })
+        if (Array.isArray(value)) {
+            return value
+        }
+
+        return value.split(",")
+    })
+    @IsArray()
+    @IsEnum(languagesRecord, { each: true })
+    languages: LanguageLiteral
 }
